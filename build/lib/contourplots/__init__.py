dc180a6a283e5d1c0db2440606ff79a49629dca0
sarangbhagwat <59667492+sarangbhagwat@users.noreply.github.com>
2024-06-01 14:13:01 -0500

    v 0.3.4 minor formatting changes

--- build/lib/contourplots/__init__.py
+++ build/lib/contourplots/__init__.py
@@ -6,7 +6,7 @@
 # https://github.com/sarangbhagwat/contourplots/blob/main/LICENSE
 # for license details.
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __author__ = 'Sarang S. Bhagwat'
 
 # %% Initialize ContourPlots 
