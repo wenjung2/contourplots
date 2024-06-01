faf975a27edd50a55a379a3d94108ef16e823a5e
sarangbhagwat <59667492+sarangbhagwat@users.noreply.github.com>
2024-06-01 14:08:39 -0500

    change animated contourplot axis ticks to always appear above plot contents

--- contourplots/__init__.py
+++ contourplots/__init__.py
@@ -6,7 +6,7 @@
 # https://github.com/sarangbhagwat/contourplots/blob/main/LICENSE
 # for license details.
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __author__ = 'Sarang S. Bhagwat'
 
 # %% Initialize ContourPlots 
